53c936babdce97b10493bdc35f61675d6fed8797
msb0814 <1033530438@qq.com>
2023-06-14 15:11:30 +0800

    -update: Deleted Pinia.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 `pnpm i`
 `pnpm run dev`
+
+#### 技术栈:
+
+html5 + css3 + es6 + vue3 + typescript
+因项目体量较小 ,未使用全局状态管理工具
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
 import 'normalize.css';
 import Icon from '@/components/Icon.vue';
 
 const app = createApp(App);
 app.use(router);
-app.use(store);
 app.component('Icon', Icon);
 app.mount('#app');
